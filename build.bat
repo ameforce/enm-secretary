e82c1a1bf75b1ff556034f39fccf2ce1d9e72a1a
jongin.kim <enmsoftware@gmail.com>
2023-02-10 15:29:05 +0900

    feat: add automatic build function
    After that version, even if the environment is not set in conda, it is set automatically and the build proceeds

--- build.bat
+++ build.bat
@@ -1,10 +1,42 @@
+@ECHO OFF
+
 set root=C:\Users\%USERNAME%\anaconda3
+
+echo | set /p="Trying to run conda..."
 call %root%\Scripts\activate.bat %root%
+echo Success !!
 
-taskkill /f /im ENM-Secretary.exe
+echo | set /p="Changing conda's environment..."
+call conda activate enm-secretary 2> NUL
 
-call conda activate enm-secretary
-call pyinstaller -n "ENM-Secretary" --onefile --noconsole main.py
-call move "dist\ENM-Secretary.exe"
+if errorlevel 1 (
+  echo Failure
+  echo The enm-secretary environment does not exist
+  echo | set /p="Creating enm-secretary environment.."
+  call conda create -n enm-secretary python=3.11 -y > NUL 2>&1
+  echo | set /p="Creating enm-secretary environment..."
+  call conda activate enm-secretary
+)
+echo Success !!
 
+echo | set /p="Installing require python packages..."
+call pip install -r requirements.txt > NUL
+echo Success !!
+
+echo | set /p="Shutting down the running ENM-Secretary.exe process..."
+taskkill /f /im ENM-Secretary.exe > NUL 2>&1
+echo Success !!
+
+echo | set /p="Building main.py to ENM-Secretary.exe..."
+call pyinstaller -n "ENM-Secretary" --onefile --noconsole main.py > NUL 2>&1
+echo Success !!
+
+echo | set /p="Moving ENM-Secretary.exe..."
+call move "dist\ENM-Secretary.exe" > NUL
+echo Success !!
+
+echo | set /p="Running ENM-Secretary.exe..."
 start "" "ENM-Secretary.exe"
+echo Success !!
+
+
